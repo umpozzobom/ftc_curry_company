f91a1f9e15b7892e7e87230bfbd08ce74b0831fb
Ully Pozzobom <137645514+umpozzobom@users.noreply.github.com>
2023-12-04 17:34:23 -0400

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -177,9 +177,9 @@ st.sidebar.markdown("""___""")
 st.sidebar.markdown(' ## Selecione uma data limite')
 date_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.datetime(2022,4,13),
-    min_value=pd.datetime(2022,2,11),
-    max_value=pd.datetime(2022,4,6),
+    value=pd.to_datetime(2022,4,13),
+    min_value=pd.to_datetime(2022,2,11),
+    max_value=pd.to_datetime(2022,4,6),
     format='DD-MM-YYYY')
 #st.header(date_slider)
 st.sidebar.markdown("""___""")
